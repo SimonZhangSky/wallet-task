36ec2357bfc336a59b8014bd0ee9792c8f315cec
Simon Zhang <dmfh294597341@gmail.com>
2024-05-10 14:11:06 +0800

    Init project

---
+++ src/main/java/com/pundix/wallet/task/WalletTaskApplication.java
@@ -0,0 +1,11 @@
+package com.pundix.wallet.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletTaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WalletTaskApplication.class, args);
+    }
+}
